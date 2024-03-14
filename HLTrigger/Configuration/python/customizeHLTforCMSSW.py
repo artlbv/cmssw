9f4893f0c4223f7ef302542bb28f6f2440673300
Melissa Quinnan <mkq5008@gmail.com>
2024-03-14 03:22:01 +0100

    fresh commit

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -233,11 +233,30 @@ def customiseForOffline(process):
 
     return process
 
-
 def customizeHLTfor43885(process):
     for producer in producers_by_type(process, "EgammaHLTClusterShapeProducer"):
         if hasattr(producer, 'isIeta'):
             delattr(producer, 'isIeta')
+    return process
+
+def customizeHLTfor44054(process):
+    """
+    Customisation for running HLT with the updated L1 UTM and AXOL1TL condition parsing from the PR 44054
+    """
+    for producer in producers_by_type(process, "L1TGlobalProducer"):
+        if hasattr(producer, 'AXOL1TLModelVersion'):
+            delattr(producer, 'AXOL1TLModelVersion')
+    return process
+    
+def checkHLTfor43774(process):
+    filt_types = ["HLTEgammaGenericFilter","HLTEgammaGenericQuadraticEtaFilter","HLTEgammaGenericQuadraticFilter","HLTElectronGenericFilter"]
+    absAbleVar = ["DEta","deta","DetaSeed","Dphi","OneOESuperMinusOneOP","OneOESeedMinusOneOP"]
+    for filt_type in filt_types:
+        for filt in filters_by_type(process, filt_type):
+            if filt.varTag.productInstanceLabel in absAbleVar:
+                if (filt.useAbs != cms.bool(True)):
+                    print('# TSG WARNING: check value of parameter "useAbs" in',filt,'(expect True but is False)!')
+
     return process
     
 # CMSSW version specific customizations
@@ -254,6 +273,9 @@ def customizeHLTforCMSSW(process, menuType="GRun"):
     # add call to action function in proper order: newest last!
     # process = customiseFor12718(process)
 
-    process = customizeHLTfor43885(process)
+    process = checkHLTfor43774(process)
+
+    # customizes AXOL1TL condition in the L1 menu 
+    process = customizeHLTfor44054(process)
 
     return process
