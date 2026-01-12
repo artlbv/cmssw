ac819cb6393fd60a71d0d2406b764fb1ed5cd9b8
Artur Lobanov <Artur.Lobanov@cern.ch>
2026-01-12 15:22:03 +0100

    Move TOPO feature extraction to separate method

--- L1Trigger/TopoML/plugins/l1tTopoBDTProducer.cc
+++ L1Trigger/TopoML/plugins/l1tTopoBDTProducer.cc
@@ -105,7 +105,7 @@ struct GetHwQual {
 };
 
 //============================================================
-// Variable map (RESTORED)
+// Variable map
 //============================================================
 using VarFn = float (*)(const ObjectRef&);
 
@@ -117,6 +117,104 @@ static const std::unordered_map<std::string, VarFn> kVarMap = {
   {"pt",     &varPt},
   {"hwIso",  &varHwIso},
   {"hwQual", &varHwQual}
+};
+
+//============================================================
+// Object getter registry
+//============================================================
+static const std::unordered_map<std::string, ObjectGetterFn> kObjectGetters_ = {
+  {"L1Mu",  &getMu},
+  {"L1EG",  &getEG},
+  {"L1Tau", &getTau},
+  {"L1Jet", &getJet},
+  {"L1HT",  &getHT}
+};
+
+//============================================================
+// Feature definition
+//============================================================
+struct Feature {
+  ObjectGetterFn getObj;
+  VarFn          getVar;
+  unsigned       index;
+};
+
+//============================================================
+// Helper: L1TFeatureVectorExtractor
+//============================================================
+class L1TFeatureVectorExtractor {
+public:
+  L1TFeatureVectorExtractor(const edm::FileInPath& jsonPath, int bx)
+    : bx_(bx)
+  {
+    std::ifstream f(jsonPath.fullPath());
+    if(!f) throw cms::Exception("Configuration") << "Cannot open JSON file: " << jsonPath.fullPath() << "\n";
+
+    nlohmann::json j;
+    f >> j;
+    const auto& fmap = j.at("feature_map");
+
+    nFeatures_ = fmap.size();
+    features_.resize(nFeatures_);
+
+    for(auto& [name, idx] : fmap.items()) {
+      auto p1 = name.find('_');
+      auto p2 = name.find('_', p1+1);
+      std::string obj = name.substr(0,p1);
+      unsigned index = std::stoi(name.substr(p1+1, p2-p1-1));
+      std::string var = name.substr(p2+1);
+
+      auto itObj = kObjectGetters_.find(obj);
+      if(itObj == kObjectGetters_.end())
+        throw cms::Exception("Configuration") 
+          << "Unknown object in feature map: " << obj << " for feature " << name << "\n";
+
+      auto itVar = kVarMap.find(var);
+      if(itVar == kVarMap.end())
+        throw cms::Exception("Configuration") 
+          << "Unknown variable in feature map: " << var << " for feature " << name << "\n";
+
+      features_[idx] = {itObj->second, itVar->second, index};
+    }
+  }
+
+  std::vector<float> getFeatures(const L1Collections& cols) const {
+    std::vector<float> x(nFeatures_,0.f);
+    for(unsigned i=0; i<nFeatures_; ++i) {
+      const auto& f = features_[i];
+      ObjectRef ref = f.getObj(bx_, f.index, cols);
+      x[i] = f.getVar(ref);
+    }
+    return x;
+  }
+
+  // print loaded features name, type and index for debugging
+  void printFeatures() const {
+    std::cout << "Loaded features: " << std::endl;
+    for (unsigned i = 0; i < features_.size(); ++i) {
+      const auto& f = features_[i];
+      std::string objName;
+      for (const auto& [name, fn] : kObjectGetters_) {
+        if (fn == f.getObj) {
+          objName = name;
+          break;
+        }
+      }
+      std::string var;
+      for (const auto& [name, fn] : kVarMap) {
+        if (fn == f.getVar) {
+          var = name;
+          break;
+        }
+      }
+      std::cout << "  Feature " << i << ": object type '" << objName << "', index " << f.index << ", variable '" << var << "'" << std::endl;
+    }
+  }
+
+private:
+  int bx_;
+  unsigned nFeatures_;
+  std::vector<Feature> features_;
 };
 
 //============================================================
@@ -138,6 +236,7 @@ private:
   edm::EDGetTokenT<BXVector<l1t::Jet>> jetToken_;
   edm::EDGetTokenT<BXVector<l1t::EtSum>> sumToken_;
 
+  bool debug_;
   // std::string modelPath;
   int bx_;
 
@@ -145,123 +244,39 @@ private:
   using BDT_t = conifer::BDT<float, float, true>;
   std::unique_ptr<BDT_t> bdt_;
 
-  // feature accessors
-  struct Feature {
-    ObjectGetterFn getObj;
-    VarFn          getVar;
-    unsigned       index;
-  };
-  static const std::unordered_map<std::string, ObjectGetterFn> kObjectGetters_;
-
-  std::vector<Feature> features_;
-  unsigned nFeatures_;
-
-};
-
-//============================================================
-// Object getter registry
-//============================================================
-const std::unordered_map<std::string, ObjectGetterFn>
-L1TTopoBDTProducer::kObjectGetters_ = {
-  {"L1Mu",  &getMu},
-  {"L1EG",  &getEG},
-  {"L1Tau", &getTau},
-  {"L1Jet", &getJet},
-  {"L1HT",  &getHT}
+  L1TFeatureVectorExtractor featureExtractor_;
 };
 
 // =====================================================
 // Constructor
 // =====================================================
 
-L1TTopoBDTProducer::L1TTopoBDTProducer(const edm::ParameterSet& cfg) {
+L1TTopoBDTProducer::L1TTopoBDTProducer(const edm::ParameterSet& cfg) :
+  featureExtractor_(cfg.getParameter<edm::FileInPath>("model_path"), cfg.getParameter<int>("bx"))
+  {
   muToken_ = consumes<l1t::MuonBxCollection>(cfg.getParameter<edm::InputTag>("muToken"));
   egToken_ = consumes<l1t::EGammaBxCollection>(cfg.getParameter<edm::InputTag>("egToken"));
   tauToken_ = consumes<l1t::TauBxCollection>(cfg.getParameter<edm::InputTag>("tauToken"));
   jetToken_ = consumes<l1t::JetBxCollection>(cfg.getParameter<edm::InputTag>("jetToken"));
   sumToken_ = consumes<l1t::EtSumBxCollection>(cfg.getParameter<edm::InputTag>("etSumToken"));
   bx_ = cfg.getParameter<int>("bx");
-
-  edm::FileInPath modelPath(cfg.getParameter<std::string>("model_path"));
+  debug_ = cfg.getParameter<bool>("debug");
+  edm::FileInPath modelPath = cfg.getParameter<edm::FileInPath>("model_path");
   
   produces<float>("score");
-  // produces<nanoaod::FlatTable>("scores");
 
   // ---- Load JSON: Model and feature names --------------------------------------------
 
   // print model path for debugging
-  std::cout << "Loading BDT model from: " << modelPath.fullPath() << std::endl;
-
+  if (debug_) {
+    std::cout << "Loading BDT model and features from JSON: " << modelPath.fullPath() << std::endl;
+    featureExtractor_.printFeatures();
+  }
   // Load BDT model
   try {
     bdt_ = std::make_unique<BDT_t>(modelPath.fullPath());
   } catch (const std::exception& e) {
     throw cms::Exception("L1TTopoBDTProducer") << "Error loading BDT model from " << modelPath << ": " << e.what();
-  }
-  
-  // read the model json to get feature names
-  std::ifstream ifs(modelPath.fullPath());
-  nlohmann::json json;
-  ifs >> json;
-
-  const auto& fmap = json.at("feature_map");
-  nFeatures_ = fmap.size();
-  features_.resize(nFeatures_);
-
-  for (auto& [name, idx] : fmap.items()) {
-    auto p1 = name.find('_');
-    auto p2 = name.find('_', p1 + 1);
-
-    std::string obj  = name.substr(0, p1);
-    unsigned index   = std::stoi(name.substr(p1 + 1, p2 - p1 - 1));
-    std::string var  = name.substr(p2 + 1);
-
-    auto itObj = kObjectGetters_.find(obj);
-    if (itObj == kObjectGetters_.end()) {
-        throw cms::Exception("Configuration")
-          << "Unknown object type in feature map: '" << obj
-          << "' for feature '" << name << "'.\n"
-          << "Available object types: ";
-        for (const auto& kv : kObjectGetters_) std::cout << kv.first << " ";
-        std::cout << "\nPlease prefix with L1 if needed (e.g. 'L1HT_0_pt').";
-    }
-
-    auto itVar = kVarMap.find(var);
-    if (itVar == kVarMap.end()) {
-        throw cms::Exception("Configuration")
-          << "Unknown variable in feature map: '" << var
-          << "' for feature '" << name << "'.\n"
-          << "Available variable names: ";
-        for (const auto& kv : kVarMap) std::cout << kv.first << " ";
-        std::cout << "\n";
-    }
-
-    // assign safely
-    auto og = itObj->second;
-    auto vf = itVar->second;
-
-    features_[idx] = {og, vf, index};
-  }
-
-  // print loaded features name, type and index for debugging
-  std::cout << "Loaded features: " << std::endl;
-  for (unsigned i = 0; i < features_.size(); ++i) {
-    const auto& f = features_[i];
-    std::string objName;
-    for (const auto& [name, fn] : kObjectGetters_) {
-      if (fn == f.getObj) {
-        objName = name;
-        break;
-      }
-    }
-    std::string var;
-    for (const auto& [name, fn] : kVarMap) {
-      if (fn == f.getVar) {
-        var = name;
-        break;
-      }
-    }
-    std::cout << "  Feature " << i << ": object type '" << objName << "', index " << f.index << ", variable '" << var << "'" << std::endl;
   }
 }
 
@@ -276,6 +291,7 @@ void L1TTopoBDTProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSet
   edm::Handle<BXVector<l1t::Tau>> taus;
   edm::Handle<BXVector<l1t::Jet>> jets;
   edm::Handle<BXVector<l1t::EtSum>> sums;
+  
   iEvent.getByToken(muToken_, muons);
   iEvent.getByToken(egToken_, egammas);
   iEvent.getByToken(tauToken_, taus);
@@ -283,30 +299,25 @@ void L1TTopoBDTProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSet
   iEvent.getByToken(sumToken_, sums);
 
   L1Collections cols{*muons, *egammas, *taus, *jets, *sums};
-
-  std::vector<float> features_x(nFeatures_, 0.f);
-
-  for (unsigned i = 0; i < nFeatures_; ++i) {
-    const auto& f = features_[i];
-    ObjectRef ref = f.getObj(bx_, f.index, cols);
-    features_x[i] = f.getVar(ref);
-  }
-
-  // print features for debugging
-  std::cout << "Features: ";
-  for (const auto& f : features_x) {
-    std::cout << f << " ";
-  } 
-  std::cout << std::endl;
-
+  std::vector<float> features_x = featureExtractor_.getFeatures(cols);
   std::vector<float> output = bdt_->decision_function(features_x);
 
-  // print score
-  std::cout << "BDT score: " << output.at(0) << std::endl;
+  if (debug_) {
+    // print features and BDT score for debugging
+    std::cout << "Features: ";
+    for (const auto& f : features_x) {
+      std::cout << f << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "BDT score: " << output.at(0) << std::endl;
+    // for (size_t i = 0; i < output.size(); ++i) {
+    //   std::cout << "  Output[" << i << "] = " << output[i] << std::endl;
+    // }
+  }
 
   // store score in event
   iEvent.put(std::make_unique<float>(output.at(0)), "score");
-  // iEvent.put(std::make_unique<float>(output.at(0)));
 }
 
 // make fillDescriptions
@@ -318,8 +329,9 @@ void L1TTopoBDTProducer::fillDescriptions(edm::ConfigurationDescriptions& descri
   desc.add<edm::InputTag>("tauToken", edm::InputTag("l1tTau"));
   desc.add<edm::InputTag>("jetToken", edm::InputTag("l1tJet"));
   desc.add<edm::InputTag>("etSumToken", edm::InputTag("l1tEtSum"));
-  desc.add<int>("bx", 0);
-  desc.add<std::string>("model_path", "L1Trigger/TopoML/data/bdt_model.json");
+  desc.add<int>("bx", 0)->setComment("BX to process");
+  desc.add<edm::FileInPath>("model_path", edm::FileInPath("L1Trigger/TopoML/data/conifer_model_HH2b2t_2recotauh.json"))->setComment("Path to BDT model JSON file");
+  desc.add<bool>("debug", false)->setComment("Enable debug printouts");
   
   descriptions.addWithDefaultLabel(desc);
 }
